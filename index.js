87d1d4092efd1d2fc1154284614bd25e676cd64e
juicygranpa <juicygranpa@juicygranpa-mac.local>
2017-09-05 21:41:23 -0700

    upgrade to pool

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.post('/webhook/', function (req, res) {
 app.get('/db', function (request, response) {
   
   var pool = new pg.Pool()
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  pool.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
       if (err)
