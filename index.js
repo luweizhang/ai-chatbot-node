fe0e8dbba71810bdde6f32d6cef19bdbb557f06f
juicygranpa <juicygranpa@juicygranpa-mac.local>
2017-09-05 21:23:20 -0700

    fix bug i think

--- index.js
+++ index.js
@@ -7,17 +7,7 @@ const pg = require('pg'); //postgresql
 
 const app = express()
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-});
+
 
 app.set('port', (process.env.PORT || 5000))
 // parse application/x-www-form-urlencoded
@@ -255,5 +245,16 @@ More features to add in the future:
 
 */
 
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
 
 
